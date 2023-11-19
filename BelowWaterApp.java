9ff623f9ec9f1c64e418b0bdcd67101e6315145d
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2023-11-19 21:16:31 +0000

    Add files via upload

---
+++ BelowWaterApp.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package xpath.belowwaterapp;
+
+
+/**
+ * BelowWaterApp
+ * @author niamh
+ * 26/10/2023
+ */
+
+public class BelowWaterApp {
+    public static void main(String[] args) {
+        WaterGUI myWaterGUI = new WaterGUI();
+        myWaterGUI.setVisible(true);
+    }
+}
